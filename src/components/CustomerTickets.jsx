eef7ac11d6062803ea98df4a6812d5fb27715bbc
ShaheD922 <shahedhasan.yc@gmail.com>
2025-09-26 22:37:30 +0600

    added a taskpart and functionality

--- src/components/CustomerTickets.jsx
+++ src/components/CustomerTickets.jsx
@@ -3,7 +3,7 @@ const CustomerTickets = ({ ticketss, handleCardClick }) => {
   return (
     <div>
       <p className="  text-2xl md:text-4xl font-bold">Customer Tickets</p>
-      <div className=" grid md:grid-cols-2 gap-4 mt-7">
+      <div className=" shadow-md grid md:grid-cols-2 gap-4 mt-7">
         {ticketss.map((ticket) => (
           <TicketCard
             key={ticket.id}
--- src/components/TicketCard.jsx
+++ src/components/TicketCard.jsx
@@ -2,28 +2,28 @@ const TicketCard = ({ ticket, handleCardClick }) => {
   return (
     <div
       onClick={() => handleCardClick(ticket)}
-      className="border-2 border-gray-300  card bg-gradient-to-r from-violet-400 via-purple-300 to-indigo-400 w-full shadow-sm cursor-pointer p-[20px] rounded-3xl"
+      className="border-2  border-gray-300  card bg-gradient-to-r from-violet-400 via-purple-300 to-indigo-400 w-full shadow-md cursor-pointer p-[20px] rounded-3xl"
     >
       <div className="card-body">
         <div className="grid grid-cols-2 items-stretch">
-          <h2 className="card-title">{ticket.title}</h2> 
+          <h2 className="card-title font-semibold text-gray-600">{ticket.title}</h2> 
         <p
   className={`justify-self-end ${
-    ticket.status === "Open" ? "bg-violet-500  font-semibold p-1 rounded text-white" : ""
+    ticket.status === "Open" ? "bg-violet-500  font-semibold p-1 rounded-[20px] text-white" : ""
   } ${ticket.status === "In-Progress" ? "bg-indigo-300 font-semibold text-white p-1 rounded-[20px]" : ""}`}
 >
   {ticket.status}
 </p>
         </div>
-        <p>{ticket.description}</p>
+        <p className=" font-semibold py-2 text-gray-600">{ticket.description}</p>
         <div className="flex justify-between">
           <div className="flex gap-4">
-            <p>{ticket.id}</p>
+            <p className=" font-semibold text-gray-800">{ticket.id}</p>
             <p
               className={`${
-                ticket.priority === "High" ? "text-red-600" : ""
-              } ${ticket.priority === "Medium" ? "text-yellow-500" : ""} ${
-                ticket.priority === "Low" ? "text-green-700" : ""
+                ticket.priority === "High" ? "text-red-800 font-bold px-2 " : ""
+              } ${ticket.priority === "Medium" ? "text-orange-500 font-bold px-2" : ""} ${
+                ticket.priority === "Low" ? "text-green-800 font-bold px-2" : ""
               }`}
             >
               {ticket.priority}
@@ -31,8 +31,8 @@ const TicketCard = ({ ticket, handleCardClick }) => {
           </div>
 
           <div className="flex gap-4">
-            <p>{ticket.assignee}</p>
-            <p className="flex gap-1 items-center">
+            <p className="">{ticket.assignee}</p>
+            <p className="flex gap-1 items-center font-semibold text-gray-600">
               📅 {ticket.date}
             </p>
           </div>
@@ -43,6 +43,4 @@ const TicketCard = ({ ticket, handleCardClick }) => {
 };
 
 export default TicketCard;
-
-//
 
