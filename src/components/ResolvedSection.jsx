a73278adfbfc05b65f8330a476556e6aa2aae9bb
ShaheD922 <shahedhasan.yc@gmail.com>
2025-09-27 15:47:11 +0600

    update resolve section to conditional rendaring

--- src/components/ResolvedSection.jsx
+++ src/components/ResolvedSection.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ResolvedCard from './ResolvedCard';
-const ResolvedSection = ({resolvedData}) => {
-    return (
-       <div className="mt-12">
+
+const ResolvedSection = ({ resolvedData }) => {
+  return (
+    <div className="mt-12">
       <p className="text-2xl md:text-3xl font-semibold my-2">Resolved Task</p>
-      <p className='text-gray-400'>Number of resolved tasks.</p>
+      {resolvedData.length === 0 && (
+        <p className="text-gray-400">No resolved tasks yet.</p>
+      )}
       <div className="grid gap-y-3 mt-3 mb-4">
         {resolvedData.map((resolve) => (
           <ResolvedCard key={resolve.id} resolve={resolve} />
         ))}
       </div>
     </div>
-    );
+  );
 };
 
 export default ResolvedSection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// sm
